b563fad84faed0ee72ad3487c44445fb3de12c1c
Simcodes <segunmichael24@gmail.com>
2025-08-28 22:04:26 +0100

    Update ALLOWED_HOSTS in settings.py for local and production environments

--- auth_service/settings.py
+++ auth_service/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://intern-task-uw5x.onrender.com']
 
 
 # Application definition
